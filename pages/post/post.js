f7766c47f1d5d67ba43a7cefd8128b6caa4a456f
HYC <461892863@qq.com>
2017-09-26 15:47:41 +0800

    diyici

---
+++ pages/post/post.js
@@ -0,0 +1,15 @@
+Page({
+  data: {
+    message: 'Hello MINA!'
+  },
+  onLoad:function(){
+    var post_content1 = {
+      date:"Nov 12 2016",
+      title:"今儿个天儿不错",
+      post_image:"/images/3.jpg"
+    }
+    this.setData(post_content1);
+
+
+  }
+})
