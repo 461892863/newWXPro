047c7f610194f018568b4948a5f0306bd8b5039a
heyucheng <461892863@qq.com>
2017-09-27 10:12:27 +0800

    2017/9/27

--- pages/post/post.js
+++ pages/post/post.js
@@ -2,16 +2,17 @@ Page({
   data: {
     message: 'Hello MINA!',
     data:"这是data",
-    app:"app的内容"
+    app:"app的内容",
+    post_image: "/images/3.jpg"
+    
   },
   onLoad:function(){
     var post_content1 = {
       date:"Nov 12 2016",
       title:"今儿个天儿不错",
-      post_image:"/images/3.jpg"
+      post_image1:"/images/4.jpg"
     }
     this.setData(post_content1);
-
-
   }
+  
 })
